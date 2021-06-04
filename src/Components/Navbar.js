33993eb8a869c5785a98c27f5cebd8aaf1b54718
ethan herring <ethanlherring@gmail.com>
2021-06-04 17:58:47 -0500

    Updated some code before deploy.

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,43 +1,76 @@
 import React, { Component } from 'react';
 import { Link } from 'react-scroll';
+import { Sling as Hamburger } from 'hamburger-react';
 
 class Navbar extends Component {
     state = {};
+    constructor(props, context) {
+        super(props, context);
+       
+        this.state = {
+          visible: false
+        };
+       
+        this.toggleMenu = this.toggleMenu.bind(this);
+      }
+       
+      toggleMenu() {
+        this.setState({
+            visible: !this.state.visible
+        });
+      }
     render () {
         return(
             <div className="navbar">
+                
+                <Hamburger easing="ease-in"
+                    onToggle={this.toggleMenu}
+                    
+                    color="#666699" />
+                
                     <div className="links">
-                        <Link
-                        activeClass="active"
-                        to="main"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                        >
-                            Main
-                        </Link>
-                        <Link
-                        activeClass="active"
-                        to="about"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                        >
-                            About
-                        </Link>
-                        <Link
-                        activeClass="active"
-                        to="contact"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={500}
-                        >
-                            Contact
-                        </Link>
+                        <ul>
+                            <li>
+                                <Link
+                                activeClass="active"
+                                to="main"
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={500}
+                                >
+                                    Main
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                activeClass="active"
+                                to="about"
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={500}
+                                >
+                                    About
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                activeClass="active"
+                                to="contact"
+                                spy={true}
+                                smooth={true}
+                                offset={0}
+                                duration={500}
+                                >
+                                    Contact
+                                </Link>
+                            </li>
+                        </ul>
+                            
+                        
                     </div>
+                    
             </div>
         );
     }
