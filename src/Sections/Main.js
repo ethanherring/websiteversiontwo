5d6ec7b3fc5a1881842c34449cb5d2f620d709b2
ethan herring <ethanlherring@gmail.com>
2021-06-06 14:05:06 -0500

    testing react-ga for google analytics stream

--- src/Sections/Main.js
+++ src/Sections/Main.js
@@ -1,13 +1,11 @@
 import { React, Component } from 'react';
-import ReactGA from 'react-ga';
+
 
 class Main extends Component {
     state = {};
     
 
     render() {
-        ReactGA.initialize('G-LN1KCMZQFC');
-        ReactGA.pageview(window.location.pathname + window.location.search);
         return (
             <div className="main-section" id="main">
                 <div className="main-text" id="main-text">
